d3889907092bf7033435979ac1f83e756d2bb892
Aleksey V Zapparov <ixti@member.fsf.org>
2012-05-01 11:59:28 +0200

    Implement assets builder

--- test/demo.js
+++ test/demo.js
@@ -10,7 +10,7 @@ env.appendPath('app/assets/stylesheets');
 env.appendPath('app/vendor/stylesheets');
 
 
-env.findAsset('app.js').getSource(function (err, data) {
-  console.log('\n### Error of app.js:\n' + err);
-  console.log('\n### Contents of app.js:\n' + data);
+env.findAsset('app.css').getSource(function (err, data) {
+  console.log('\n### Error of app.css:\n' + err);
+  console.log('\n### Contents of app.css:\n' + data);
 });
