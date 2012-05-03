b7dfc12b2d732a1daebb844a7b40ecf3e167c0a0
Aleksey V Zapparov <ixti@member.fsf.org>
2012-05-03 20:43:27 +0200

    Fix formatExtension guesser

--- test/demo.js
+++ test/demo.js
@@ -5,15 +5,17 @@ var mincer = require('../index');
 var env = new (mincer.Environment)(__dirname + '/fixtures');
 
 
+env.appendPath('app/assets/images');
 env.appendPath('app/assets/javascripts');
 env.appendPath('app/assets/stylesheets');
 env.appendPath('vendor/assets/stylesheets');
+env.appendPath('vendor/assets/javascripts');
 
 
 var manifest = new (mincer.Manifest)(env, __dirname + '/assets');
 
 
-manifest.compile(['app.css', 'app.js'], function (err, manifest) {
+manifest.compile(['app.css', 'app.js', 'header.jpg'], function (err, manifest) {
   if (err) {
     console.error(err);
     return;
--- test/fixtures/app/assets/javascripts/app.js
+++ test/fixtures/app/assets/javascripts/app.js
@@ -1,3 +1,4 @@
 //= require foobar
 //= require test
+//= require jquery.collapser
 alert(123)
