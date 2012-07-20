b61f6d33c4b78c14230df5b9274f9e3888376aeb
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-20 17:31:36 +0200

    Update isFresh signatures

--- test/issues/issue-13.js
+++ test/issues/issue-13.js
@@ -48,15 +48,15 @@ write('bar.js', '/*' + Date.now() + '*/');
 
 
 env.findAsset('foo').compile(function () {
-  report('foo should be fresh', env.findAsset('foo').isFresh());
+  report('foo should be fresh', env.findAsset('foo').isFresh(env));
 
   write('bar.js', '/*' + Date.now() + '*/');
-  report('foo should be stale', !env.findAsset('foo').isFresh());
+  report('foo should be stale', !env.findAsset('foo').isFresh(env));
 
   env.findAsset('foo').compile(function () {
-    report('foo should be fresh', env.findAsset('foo').isFresh());
+    report('foo should be fresh', env.findAsset('foo').isFresh(env));
 
     write('foo.js', '/*' + Date.now() + '*/\n//= require bar');
-    report('foo should be stale', !env.findAsset('foo').isFresh());
+    report('foo should be stale', !env.findAsset('foo').isFresh(env));
   });
 });
