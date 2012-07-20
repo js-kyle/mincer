40195bd42eace31f4c197ffdf69a8ee1d305097c
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-20 22:14:30 +0200

    Fix issue #13 test

--- test/issues/issue-13.js
+++ test/issues/issue-13.js
@@ -50,13 +50,15 @@ write('bar.js', '/*' + Date.now() + '*/');
 env.findAsset('foo').compile(function () {
   report('foo should be fresh', env.findAsset('foo').isFresh(env));
 
-  write('bar.js', '/*' + Date.now() + '*/');
-  report('foo should be stale', !env.findAsset('foo').isFresh(env));
-
-  env.findAsset('foo').compile(function () {
-    report('foo should be fresh', env.findAsset('foo').isFresh(env));
-
-    write('foo.js', '/*' + Date.now() + '*/\n//= require bar');
+  setTimeout(function () {
+    write('bar.js', '/*' + Date.now() + '*/');
     report('foo should be stale', !env.findAsset('foo').isFresh(env));
-  });
+
+    env.findAsset('foo').compile(function () {
+      report('foo should be fresh', env.findAsset('foo').isFresh(env));
+
+      write('foo.js', '/*' + Date.now() + '*/\n//= require bar');
+      report('foo should be stale', !env.findAsset('foo').isFresh(env));
+    });
+  }, 1234);
 });
