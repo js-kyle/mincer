c51e59ba77c0f3617c6c2c9fedd1fbd7f83d03b4
Vaughan Rouesnel <vrouesnel@gmail.com>
2013-04-19 09:00:42 +1000

    Fix tests.

--- test/environment.js
+++ test/environment.js
@@ -1,20 +1,20 @@
+/*global describe, it, before*/
+
 'use strict';
 
-var assert  = require('assert')
-  , Mincer  = require('../lib/mincer');
+var Mincer  = require('../lib/mincer')
+  , env;
 
-var env;
-
-var precompile = function(cached, done) {
+function precompile(cached, done) {
   var cb = function(err, data) {
-    if (err) return done(err);
-    if (data.assets['precompile_test.js'] != null) {
-      done()
+    if (err) { return done(err); }
+    if (data.assets['precompile_test.js'] !== null) {
+      done();
     } else {
       done("Failed to precompile");
     }
   };
-  if (cached == false) {
+  if (cached === false) {
     env.precompile(['precompile_test.js'], cb);
   } else {
     env.precompile(['precompile_test.js'], {cachedPaths: true}, cb);
@@ -23,30 +23,30 @@ var precompile = function(cached, done) {
 
 // Compile time should be reduced by at least a factor of 5
 // when paths have been cached.
-var test_precompile = function(cached, done) {
+function test_precompile(cached, done) {
   var start1 = new Date
     , precompile1 = 0
     , precompile2 = 0;
-  precompile(cached, function(err){
-    if (err) return done(err)
+  precompile(cached, function(err) {
+    if (err) { return done(err); }
     precompile1 = new Date - start1;
     var start2 = new Date;
-    precompile(cached, function(err){
-      if (err) return done(err)
+    precompile(cached, function(err) {
+      if (err) { return done(err); }
       precompile2 = new Date - start2;
       console.log ("Uncached precompile:", precompile1, "ms");
       console.log ("Cached precompile:", precompile2, "ms");
-      var faster = precompile2 < (precompile1 / 5)
+      var faster = precompile2 < (precompile1 / 5);
       if (faster && cached) {
-        done()
+        done();
       } else if (!faster && !cached) {
-        done()
+        done();
       } else if (!faster && cached) {
-        done("Cached precompile didn't compile fast enough to be cached.")
+        done("Cached precompile didn't compile fast enough to be cached.");
       } else if (faster && !cached) {
-        done("Cached precompile compiled too fast to not be cached.")
+        done("Cached precompile compiled too fast to not be cached.");
       }
-    })
+    });
   });
 }
 
