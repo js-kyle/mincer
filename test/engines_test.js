397f187922084767df5b3159655b3509a08afabd
Aleksey V Zapparov <ixti@member.fsf.org>
2013-08-05 22:53:08 +0200

    Fix ECO template engine
    Closes #101

--- test/engines_test.js
+++ test/engines_test.js
@@ -26,11 +26,22 @@ describe("Engines", function () {
   });
 
   describe("LESS", function () {
-
     it("should support context helpers", function () {
       var asset = env.findAsset("less_engine/stylesheet");
       assert(asset.toString().match(/\/assets\/ixti-[a-f0-9]{32}.gif/));
       assert(asset.toString().match(/data:image\/gif;base64/));
+    });
+  });
+
+
+  describe("JST", function () {
+    describe("ECO", function () {
+      it("should compile to a JS function", function () {
+        var asset = env.findAsset("jst_engine/eco/template");
+
+        assert(asset.toString().match(/this\.JST/));
+        assert(asset.toString().match(/value\.ecoSafe/));
+      });
     });
   });
 
