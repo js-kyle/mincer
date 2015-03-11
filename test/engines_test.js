b5720ee8bbe02ffcd93f9bdf72a39f574d821856
Carlos Correa <ccorrea@opentable.com>
2015-03-10 19:25:17 -0700

    test: add test for node-sass importer hook.

--- test/engines_test.js
+++ test/engines_test.js
@@ -1,8 +1,27 @@
-/* global describe, it */
+/* global describe, it, after */
 
 'use strict';
 
 var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+
+function assetPath(filename) {
+  return path.join(__dirname, 'fixtures', filename);
+}
+
+function copySync(src, dest) {
+  fs.writeFileSync(dest, fs.readFileSync(src));
+}
+
+function bumpMtimeSync(pathname) {
+  var stat = fs.statSync(pathname);
+  var oneSecond = 1000;
+  // "time_t provides times accurate to one second."
+  // http://en.wikipedia.org/wiki/Stat_%28system_call%29#Time_granularity
+  var newMtime = new Date(stat.mtime.getTime() + oneSecond);
+  fs.utimesSync(pathname, stat.atime, newMtime);
+}
 
 describe('Engines', function () {
   var env = require('./environment')();
@@ -50,9 +69,27 @@ describe('Engines', function () {
   });
 
   describe('SASS', function () {
+    var variablesPath = assetPath('sass_engine/_variables.css.scss');
+
+    after(function() {
+      fs.unlinkSync(variablesPath);
+    });
+
     it('should process stylesheets', function () {
+      copySync(assetPath('sass_engine/_variables.css.scss.v1'), variablesPath);
+
       var asset = env.findAsset('sass_engine/stylesheet');
       assert(asset.toString().match(/\.foo\s+\.bar\s*\{/));
+      assert(asset.toString().match(/\.column\s+\{\s+width:\s*30px/));
+    });
+
+    it('should process changes to stylesheets', function () {
+      copySync(assetPath('sass_engine/_variables.css.scss.v2'), variablesPath);
+      bumpMtimeSync(variablesPath);
+
+      var asset = env.findAsset('sass_engine/stylesheet');
+      assert(asset.toString().match(/\.foo\s+\.bar\s*\{/));
+      assert(asset.toString().match(/\.column\s+\{\s+width:\s*40px/));
     });
   });
 });
